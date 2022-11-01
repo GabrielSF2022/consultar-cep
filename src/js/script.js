fd3da53a268a43e142d1eef02eab45cf07174900
GABRIEL DE SOUZA FURTADO <97768716+GabrielSF2022@users.noreply.github.com>
2022-11-01 13:34:58 -0300

    Finalizando Projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,46 @@
 const cep = document.querySelector("#cep");
+const button = document.querySelector("#button")
+const result = document.querySelector(".result")
+const result_ivalid = document.querySelector(".container-result-invalid") 
 
-cep.addEventListener("blur",(e)=>{
-    let search = cep.value.replace("-","")
-    const options = {
-        method : 'GET',
-        mode: 'cors',
-        cache: 'default'
+const showData = (result)=>{
+    for(const campo in result){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = result[campo]
+        }
+        
+    }
+}
+
+button.addEventListener("click",(e)=>{
+    if(cep.value === ''){
+
+        result_ivalid.classList.add("red")
+        result_ivalid.textContent = 'CEP INVALIDO!'
+
+        setTimeout(()=>{
+            result_ivalid.textContent  = ''
+        },3000)
+    }
+    else{
+        let search = cep.value.replace("-","")
+        const options = {
+            method : 'GET',
+            mode: 'cors',
+            cache: 'default'
+        }
+
+        fetch(`https://viacep.com.br/ws/${search}/json`)
+        .then((response)=> {response.json().
+            then( data => showData(data))
+        })
+
+        .catch(e => console.log('Deu Erro: '+ e,message))
+        console.log(cep.value + "dsfsd")
+
+        result.classList.add('open')
+
     }
 
-    fetch(`https://viacep.com.br/ws/${search}/json`)
-    .then((response)=> {response.json().
-        then( data => console.log(data))
-    })
-
-    .catch(e => console.log('Deu Erro: '+ e,message))
-    console.log(cep.value)
 })
+
